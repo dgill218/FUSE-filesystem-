5a8bdca0130000c1b1f8fedf623d2a69632fdeb5
dgill218 <gilligan.d@northeastern.edu>
2022-04-25 19:20:09 -0400

    d

--- nufs-cs3650-master/inode.c
+++ nufs-cs3650-master/inode.c
@@ -7,7 +7,7 @@
 #include "bitmap.h"
 
 void
-print_inode(inode_t* node) {
+print_inode(inode_t *node) {
     printf("inode_t located at %p:\n", node);
     printf("Reference count: %d\n", node->refs);
     printf("Node Permission + Type: %d\n", node->mode);
@@ -16,14 +16,14 @@ print_inode(inode_t* node) {
     printf("Node indirect pointer: %d\n", node->iptr);
 }
 
-inode_t*
+inode_t *
 get_inode(int inum) {
-    inode_t* inodes = get_inode_bitmap() + 32;
+    inode_t *inodes = get_inode_bitmap() + 32;
     return &inodes[inum];
 }
 
 // take a look at the malloc implementation
-int 
+int
 alloc_inode() {
     int nodenum;
     for (int i = 0; i < 256; ++i) {
@@ -33,7 +33,7 @@ alloc_inode() {
             break;
         }
     }
-    inode_t* new_node = get_inode(nodenum);
+    inode_t *new_node = get_inode(nodenum);
     new_node->refs = 1;
     new_node->size = 0;
     new_node->mode = 0;
@@ -43,25 +43,25 @@ alloc_inode() {
 }
 
 // marks the inode_t as free in the bitmap and then clears the pointer locations
-void 
+void
 free_inode(int inum) {
-    inode_t* node = get_inode(inum);
-    void* bmp = get_inode_bitmap(); 
+    inode_t *node = get_inode(inum);
+    void *bmp = get_inode_bitmap();
     shrink_inode(node, 0);
     free_block(node->dirPtrs[0]);
     bitmap_put(bmp, inum, 0);
 }
 
 // grows the inode_t, if size gets too big, it allocates a new page if possible
-int grow_inode(inode_t* node, int size) {
-    for (int i = (node->size / 4096) + 1; i <= size / 4096; i ++) {
+int grow_inode(inode_t *node, int size) {
+    for (int i = (node->size / 4096) + 1; i <= size / 4096; i++) {
         if (i < num_ptrs) { //we can use direct dirPtrs
             node->dirPtrs[i] = alloc_block(); //alloc a page
         } else { //need to use indirect
             if (node->iptr == 0) { //get a page if we don't have one
                 node->iptr = alloc_block();
             }
-            int* iptrs = blocks_get_block(node->iptr); //retrieve memory loc.
+            int *iptrs = blocks_get_block(node->iptr); //retrieve memory loc.
             iptrs[i - num_ptrs] = alloc_block(); //add another page
         }
     }
@@ -70,33 +70,31 @@ int grow_inode(inode_t* node, int size) {
 }
 
 // shrinks an inode_t by the given size
-int shrink_inode(inode_t* node, int size) {
-    for (int i = (node->size / 4096); i > size / 4096; i --) {
+int shrink_inode(inode_t *node, int size) {
+    for (int i = (node->size / 4096); i > size / 4096; i--) {
         if (i < num_ptrs) { // direct pointers
             free_block(node->dirPtrs[i]); // free
             node->dirPtrs[i] = 0;
+        } else if (i == num_ptrs) {
+            free_block(node->iptr);
+            node->iptr = 0;
         } else { // indirect pointers
-            int* iptrs = blocks_get_block(node->iptr);
+            int *iptrs = blocks_get_block(node->iptr);
             free_block(iptrs[i - num_ptrs]); // free
             iptrs[i - num_ptrs] = 0;
-
-            if (i == num_ptrs) {
-                free_block(node->iptr);
-                node->iptr = 0;
-            }
         }
-    } 
+    }
     node->size = size;
-    return 0;  
+    return 0;
 }
 
 // gets the page number for the given inode
-int inode_get_pnum(inode_t* node, int fpn) {
+int inode_get_pnum(inode_t *node, int fpn) {
     int blockNum = fpn / 4096;
     if (blockNum < num_ptrs) {
         return node->dirPtrs[blockNum];
     } else {
-        int* iptrs = blocks_get_block(node->iptr);
+        int *iptrs = blocks_get_block(node->iptr);
         return iptrs[blockNum - num_ptrs];
     }
 }
