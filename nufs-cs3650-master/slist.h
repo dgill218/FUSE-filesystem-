55fa59208f76e55b0c4b9db265be1484b3c3b0d3
dgill218 <gilligan.d@northeastern.edu>
2022-04-27 21:13:17 -0400

    pu

--- nufs-cs3650-master/slist.h
+++ nufs-cs3650-master/slist.h
@@ -3,15 +3,21 @@
 #ifndef SLIST_H
 #define SLIST_H
 
+// Used for directory listings and manipulating paths
 typedef struct slist {
     char* data;
     int   refs;
     struct slist_t* next;
 } slist_t;
 
-slist_t* s_cons(const char* text, slist_t* rest);
-void   s_free(slist_t* xs);
-slist_t* s_explode(const char* text, char delim);
+// Cons a string to a string list.
+slist_t *s_cons(const char *text, slist_t *rest);
+
+// Free the given string list.
+void s_free(slist_t *xs);
+
+// Split the given on the given delimiter into a list of strings.
+slist_t *s_explode(const char *text, char delim);
 
 
 #endif
