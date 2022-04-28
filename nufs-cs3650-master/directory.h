0c502718a6ee0ab473d6a5ba2846926483c9816a
dgill218 <gilligan.d@northeastern.edu>
2022-04-27 21:26:55 -0400

    adsf

--- nufs-cs3650-master/directory.h
+++ nufs-cs3650-master/directory.h
@@ -15,9 +15,7 @@ typedef struct dirent {
     char used; // is this directory entry being used?
 } dirent_t;
 
-// not sure what this does
-void directory_init(); 
-int directory_lookup(inode_t* dd, const char* name);
+void directory_init();
 int tree_lookup(const char* path);
 int directory_put(inode_t* dd, const char* name, int inum);
 int directory_delete(inode_t* dd, const char* name);
