7849645418f6a7c4010780984ac8bf9201cdd957
dgill218 <gilligan.d@northeastern.edu>
2022-04-27 21:42:18 -0400

    asdf

--- nufs-cs3650-master/directory.h
+++ nufs-cs3650-master/directory.h
@@ -18,12 +18,12 @@ typedef struct dirent {
 #define DIR_SIZE sizeof(dirent_t)
 
 void directory_init();
-int directory_lookup(inode_t* dd, const char* name);
-int tree_lookup(const char* path);
-int directory_put(inode_t* dd, const char* name, int inum);
-int directory_delete(inode_t* dd, const char* name);
-slist_t* directory_list(const char* path);
-void print_directory(inode_t* dd);
+int directory_lookup(inode_t *dd, const char *name);
+int tree_lookup(const char *path);
+int directory_put(inode_t *dd, const char *name, int inum);
+int directory_delete(inode_t *dd, const char *name);
+slist_t *directory_list(const char *path);
+void print_directory(inode_t *dd);
 
 #endif
 
